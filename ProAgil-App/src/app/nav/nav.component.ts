ccb54079c48befbab66b955b71ffa6269c4e2d34
jefersonfsilva <jefersonfs@gmail.com>
2020-11-13 18:43:05 -0300

    Alteração de dashboard e deploy

--- ProAgil-App/src/app/nav/nav.component.ts
+++ ProAgil-App/src/app/nav/nav.component.ts
@@ -18,6 +18,10 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
+  showMenu() {
+    return this.router.url !== '/user/login';
+  }
+
   // tslint:disable-next-line: typedef
   loggedIn() {
     return this.authService.loggedIn();
